6da8975ebc6c95263cfe641bd87363f669749d94
Perkles <scleinaldo85@gmail.com>
2022-09-04 23:04:13 -0300

    first commit esfera

---
+++ script.js
@@ -0,0 +1,13 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+const PI = 3.14159;
+
+let R = parseFloat(gets());
+
+let VOLUME = (4/3) * PI * Math.pow(R,3);
+//TODO: Complete os espaços em branco com uma possível solução para o desafio
+//Para precisão numérica utiliza-se o .toFixed(n)
+
+
+print("VOLUME =  "  +  VOLUME.toFixed(3)); 
